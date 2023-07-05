c03e683a1bcd9f8b77120de7740cdca17d5aa63a
AxelMitre <101562837+AxelMitre@users.noreply.github.com>
2023-07-05 15:28:39 -0300

    Update Cliente.java

--- POO_07/src/main/java/com/mycompany/poo_07/Cliente.java
+++ POO_07/src/main/java/com/mycompany/poo_07/Cliente.java
@@ -10,5 +10,35 @@ package com.mycompany.poo_07;
  * @author CS09
  */
 public class Cliente {
-    
+    private int dni;
+    private String nombre;
+    private String direccion;
+    private int telefono;
+
+    public Cliente(int dni, String nombre, String direccion, int telefono) {
+             this.dni = dni;
+             this.nombre = nombre;
+             this.direccion = direccion;
+             this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+     public void mostrarDatos(){
+        System.out.println("\nDNI: " +this.dni+"Nombre: " +this.nombre+ "Direccion: " +this.direccion+ "telefono: "+this.telefono );
+    }
 }
+
